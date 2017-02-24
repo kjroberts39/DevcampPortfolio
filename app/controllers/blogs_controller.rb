3811589d0d0a18683164eb8dcbafc634cde5025e
KJ Roberts <kjroberts39@hotmail.com>
2017-02-24 13:53:43 -0700

    implemented edit and update functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -41,10 +41,8 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -55,7 +53,6 @@ class BlogsController < ApplicationController
     @blog.destroy
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully deleted.' }
-      format.json { head :no_content }
     end
   end
 
