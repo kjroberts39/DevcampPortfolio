13f04781694c188c07608498f37b45c7ee13bd27
KJ Roberts <kjroberts39@hotmail.com>
2017-03-09 13:47:13 -0700

    Implemented code for authorized admin user

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -5,8 +5,12 @@ module CurrentUserConcern
 		super || guest_user
 	end
 
-	def current_user
-  	super || OpenStruct.new(name: "Guest User", first_name: "Guest" , last_name: "User" , email: "guest@email.com")
+	 def guest_user
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest
   end
-
 end
