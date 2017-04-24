0d168621650346e318011f7f6ff3aa323a4245f9
KJ Roberts <kjroberts39@hotmail.com>
2017-04-24 15:40:21 -0600

    More Content changes

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
 	end
 	
   	def set_page_defaults
-  		@page_title= "DevcampPortfolio | My Portfolio Website"
+  		@page_title= "KJ Roberts | Portfolio Website"
   		@seo_keywords = "KJ Roberts portfolio"
   	end
 end
